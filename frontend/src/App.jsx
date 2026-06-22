6ef3e6387e3f47b1527068fba49465fe983b9692
kobitgrg2418 <kobitgrg22@gmail.com>
2026-06-22 16:50:41 +0545

    added route and inmported route for every pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import home from './pages/home.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
+      <Routes>
+        <Route path='/' element={<home/>} />
+        <Route path='/cart' element={<cart/>} />
+        <Route path='/login' element={<login/>} />
+        <Route path='/placeorder' element={<placeorder/>} />
+        <Route path='/order' element={<order/>} />
+        <Route path='/product/:productID' element={<product/>} />
+        <Route path='/content' element={<content/>} />
+        <Route path='/collection' element={<collection/>} />
+        <Route path='/about' element={<about/>} />
+        
+      </Routes>
+    </div>
   )
 }
 
